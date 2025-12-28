1822b54b31d63b68670fdcd9906fc4a4fb781f20
shivanjaliyadav277-sketch <shivanjaliyadav277@gmail.com>
2025-12-28 14:30:12 +0530

    Update and rename app (2).py to app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pickle
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import time
+from sklearn.datasets import fetch_california_housing
+st.title('🏠House Price prediction using ML')
+
+
+
+
+
+
+
