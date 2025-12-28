201bea70d35d1408492515bca620dd108e0919d9
shivanjaliyadav277-sketch <shivanjaliyadav277@gmail.com>
2025-12-28 16:39:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,13 @@ scaled_X = scaler.fit_transform(X)
 
 final_value = scaler.transform([all_value])
 
-model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+@st.cache_data
+def model_run(X,y):
+  model = RandomForestRegressor()
+  model.fit(X,y)
+  return model
+  
+model = model_run(X,y)
 house_price = model.predict(final_value)[0]
 
 with st.spinner('prediction house price'):
@@ -36,6 +41,7 @@ with st.spinner('prediction house price'):
 msg = f'''house price is: $ {round(house_price*100000,2)}'''
 st.success(msg)
 st.markdown('''**Design and Developed by :anajliyadav**''')
+
 
 
 
